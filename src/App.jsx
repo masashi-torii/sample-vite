918a7679ae5c023fc2f68b5346d7110fb7032dcc
masashi-torii <kagura_m1510@icloud.com>
2025-04-21 01:44:44 +0900

    MVP2_登録をしたらフォームが初期化される(学習内容は空で学習時間は0が入る)

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,67 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+export const App = () => {
   const [count, setCount] = useState(0)
+
+  // 配列で初期化する
+  　const [records, setRecords] = useState([]);
+
+  // 内容への入力したものを表示していくための状態を保存する
+  const [inputNaiyoValue, setInputNaiyoValue] = useState("");
+
+  // 学習時間への入力したものを表示していくための状態を保存する
+  const [inputTimeValue, setInputTimeValue] = useState("");
+
+  // 記録を追加する
+  const addRecord = () => {
+    if(inputNaiyoValue && inputTimeValue){
+      const newRecord = {
+        title:inputNaiyoValue,
+        time:inputTimeValue
+      }
+      setRecords([...records, newRecord]);
+
+      // 入力フィールドをクリア
+      setInputNaiyoValue("");
+      setInputTimeValue("");
+    }
+  }
+
+
+
+  // 学習内容の入力値が変更されると呼び出される
+  const onChangeInputNaiyo = (event) => {
+    // 入力値を設定
+    setInputNaiyoValue(event.target.value);
+  }
+  
+  // 学習時間の入力値が変更されると呼び出される
+  const onChangeInputTime = (event) => {
+    // 学習内容の入力値を設定
+    setInputTimeValue(event.target.value);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + Reactだよね！?</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>学習記録一覧</h1>
+    <div>学習内容：
+    <input input="formNaiyo" type="text" name="naiyo" value={inputNaiyoValue} onChange={onChangeInputNaiyo}></input></div>
+    <div>　　学習時間：
+    <input type="number" name="studyTime" value={inputTimeValue} onChange={onChangeInputTime}></input>時間</div>
+    <button onClick={addRecord}>登録</button>
+    <div>入力されている学習内容：{inputNaiyoValue}</div>
+    <div>入力されている時間：{inputTimeValue}時間</div>
+    <h3>--一覧--</h3>
+    <div>
+    {records.map(record =>(
+      <ul key={record.title + record.time}>
+      {record.title + "　　　" + record.time + "時間"}
+    </ul>
+    ))}
+    </div>
     </>
-  )
+    )
 }
 
 export default App
